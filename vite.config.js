99c0bf79a4efd8f1e78bf03593178ef4e9345bb1
Kohin <gautamkohin@gmail.com>
2026-07-02 01:36:50 -0700

    More pages to Netlify
    Successful build, not all pages were loaded into the build. New vite.config attempt

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        calendar: resolve(__dirname, 'calendar.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        workouts: resolve(__dirname, 'workouts.html'),
+        notebook: resolve(__dirname, 'notebook.html'),
+      }
+    }
+  }
+})
